12d5fde85fa485a84ff2329c3e376aeb4b299feb
Naveen <104689352+NaveenRagunathan@users.noreply.github.com>
2025-05-01 08:57:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
-
 export default defineConfig({
   base: '/build-future-now/',
   plugins: [react()],
@@ -14,8 +13,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.html'), // use the one inside src/
-    }
+    // Remove the custom input configuration to use the default index.html in the root
   }
 });
